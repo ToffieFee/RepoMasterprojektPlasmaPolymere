98b4bc51ead89fb99ffc563d63b007105c5d63ae
Josi <felinejerg@yahoo.de>
2023-08-24 09:40:05 +0200

    collect information

--- read_xls_zugversuche.py
+++ read_xls_zugversuche.py
@@ -1,14 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-# import xlsxwriter as xls
 
 
 def read_xls_zugversuch(path, filename):
-    
-    file = path + filename + ".xls"
-    excel = pd.read_excel
-    print(excel)
+    file_ = path + filename + ".xls"
+    df = pd.read_excel(file_, sheet_name="Statistik", header=[0,1])
+    df.columns = df.columns.map(" in ".join)
+    Fmax = df["Fmax in N"]
+    eRm = df["e-Rm in mm"]
+    tmax = df["tmax in MPa"]
+    S0 = df["S0 in mm²"]
+    # 0 = Mittelwert
+    # 1 = Standardabweichung absolut
+    # 2 = Standardabweichung relativ in %
+
+    # print(df["Fmax in N"].iloc[0])
+    # print(Fmax[0])
 
 
 
